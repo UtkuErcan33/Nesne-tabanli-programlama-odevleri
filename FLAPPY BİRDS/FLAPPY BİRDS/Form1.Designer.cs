e4dde8685ca194a4d8be882e0bab2d5a051cbabf
Utku Mustafa ERCAN <utku.ercan.3368@gmail.com>
2024-10-22 01:44:26 +0300

    yeniödev

---
+++ FLAPPY BİRDS/FLAPPY BİRDS/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace FLAPPY_BİRDS
+{
+    partial class anaEkran
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.üst_boru = new System.Windows.Forms.PictureBox();
+            this.alt_boru = new System.Windows.Forms.PictureBox();
+            this.bird = new System.Windows.Forms.PictureBox();
+            this.Skor_text = new System.Windows.Forms.Label();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.üst_boru)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alt_boru)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ground
+            // 
+            this.ground.Image = global::FLAPPY_BİRDS.Properties.Resources.ground;
+            this.ground.Location = new System.Drawing.Point(-12, 464);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(795, 108);
+            this.ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ground.TabIndex = 0;
+            this.ground.TabStop = false;
+            this.ground.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // üst_boru
+            // 
+            this.üst_boru.Image = global::FLAPPY_BİRDS.Properties.Resources.pipedown;
+            this.üst_boru.Location = new System.Drawing.Point(497, -32);
+            this.üst_boru.Name = "üst_boru";
+            this.üst_boru.Size = new System.Drawing.Size(93, 129);
+            this.üst_boru.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.üst_boru.TabIndex = 0;
+            this.üst_boru.TabStop = false;
+            // 
+            // alt_boru
+            // 
+            this.alt_boru.Image = global::FLAPPY_BİRDS.Properties.Resources.pipe;
+            this.alt_boru.Location = new System.Drawing.Point(497, 330);
+            this.alt_boru.Name = "alt_boru";
+            this.alt_boru.Size = new System.Drawing.Size(93, 152);
+            this.alt_boru.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.alt_boru.TabIndex = 0;
+            this.alt_boru.TabStop = false;
+            this.alt_boru.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // bird
+            // 
+            this.bird.Image = global::FLAPPY_BİRDS.Properties.Resources.bird;
+            this.bird.Location = new System.Drawing.Point(175, 154);
+            this.bird.Name = "bird";
+            this.bird.Size = new System.Drawing.Size(92, 67);
+            this.bird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bird.TabIndex = 0;
+            this.bird.TabStop = false;
+            // 
+            // Skor_text
+            // 
+            this.Skor_text.AutoSize = true;
+            this.Skor_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Skor_text.Location = new System.Drawing.Point(189, 65);
+            this.Skor_text.Name = "Skor_text";
+            this.Skor_text.Size = new System.Drawing.Size(119, 32);
+            this.Skor_text.TabIndex = 1;
+            this.Skor_text.Text = "Skor =0";
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 20;
+            this.gametimer.Tick += new System.EventHandler(this.gametimerEvent);
+            // 
+            // anaEkran
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(784, 570);
+            this.Controls.Add(this.Skor_text);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.üst_boru);
+            this.Controls.Add(this.alt_boru);
+            this.Controls.Add(this.bird);
+            this.Name = "anaEkran";
+            this.Text = "flappy birds";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.üst_boru)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alt_boru)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bird)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird;
+        private System.Windows.Forms.PictureBox alt_boru;
+        private System.Windows.Forms.PictureBox üst_boru;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label Skor_text;
+        private System.Windows.Forms.Timer gametimer;
+    }
+}
+
---
+++ FLAPPY BİRDS/FLAPPY BİRDS/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FLAPPY_BİRDS
+{
+    public partial class anaEkran : Form
+    {
+
+        int boruHızı = 8;
+        int yerÇekimi = 10;
+        int skor = 0;
+
+        public anaEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                yerÇekimi = -10;
+            }
+        }
+
+        private void gametimerEvent(object sender, EventArgs e)
+        {
+            bird.Top += yerÇekimi;
+            alt_boru.Left -= boruHızı;
+            üst_boru.Left -= boruHızı;
+            Skor_text.Text = "Skor =" + skor;
+            if (alt_boru.Left < -150)
+            {
+                alt_boru.Left = 800;
+                skor++;
+            }
+            if (üst_boru.Left < -180)
+            {
+                üst_boru.Left = 950;
+                skor++;
+            }
+            if(bird.Bounds.IntersectsWith(alt_boru.Bounds)|| bird.Bounds.IntersectsWith(üst_boru.Bounds)|| bird.Bounds.IntersectsWith(ground.Bounds))
+            {
+                EndGame();
+            }
+            if (skor > 5)
+            {
+                boruHızı = 10;
+            }
+            if (skor > 10)
+            {
+                boruHızı = 15;
+            }
+            if (bird.Top < -25)
+            {
+                EndGame();
+            }
+
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                yerÇekimi = 10;
+            }
+        }
+        void EndGame()
+        {
+            gametimer.Stop();
+            Skor_text.Text = "GAME OVER !!!!!!!!";
+        }
+    }
+}
